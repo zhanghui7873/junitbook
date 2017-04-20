7e6f73976f220a38c3bb237e46bf7527499751d1
mary.zhang <mary.zhang@sap.com>
2017-04-20 14:20:12 +0800

    add Testsuite case by mary

---
+++ src/com/testsuite/Alltest.java
@@ -0,0 +1,19 @@
+package com.testsuite;
+import com.test.*;
+
+import static org.junit.Assert.*;
+import junit.framework.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@RunWith(Suite.class)
+@SuiteClasses({CalculatorTest.class,BankAccountTest.class})
+
+public class Alltest {
+
+	
+	
+			
+
+}
